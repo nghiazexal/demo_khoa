fc6cdf5456383e3cbf6b0b3e454d4d4c1168d640
unknown <nghiazexal@gmail.com>
2021-10-06 06:59:50 +0700

    commit 1: main

--- src/main/java/com/example/demo_khoa/controller/KhoaController.java
+++ src/main/java/com/example/demo_khoa/controller/KhoaController.java
@@ -4,6 +4,7 @@ import com.example.demo_khoa.entity.Khoa;
 import com.example.demo_khoa.service.KhoaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,8 +26,9 @@ public class KhoaController {
         return khoaService.findKhoaById(khoaId);
     }
 
+
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello!";
+        return "heeloo!";
     }
 }
